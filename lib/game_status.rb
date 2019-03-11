ccd73f1e9272868553e89d50fa0fbc02af28d3f0
Joe Muro <jmuro0321@gmail.com>
2019-03-11 15:35:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,32 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], # Row wins
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], # Column wins
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], # Diagonal wins 
+  [2, 4, 6] 
+]
+
+def won(board)
+  WIN_COMBINATIONS.each do |winning_indices|
+    board[winning_indices[0]].eqls
+  end
+  
+end 
+
+def full(board)
+end
+
+def draw(board)
+end
+
+def over(board)
+end
+
+def winner(board)
+end
